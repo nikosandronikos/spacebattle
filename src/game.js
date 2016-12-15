60168d582108c6c618e014620a19b506989df84c
Nikos Andronikos <dodgeyhack@gmail.com>
2016-12-15 15:48:56 +1100

    Refactor and generalise game management stuff.
    This allows the base Game class to be extended for specific implementations.

--- src/game.js
+++ src/game.js
@@ -1,137 +1,165 @@
-window.onload = function() {
-    // Physics updates run at a fixed frequency, independent of rendering
-    // frame rate.
-    const physicsPerSecond = 25;
-    const physicsFrameTime = 1000 / physicsPerSecond;
-    const framesBeforeDrop = 5;
+// A generic game, subclass and add specific initialisation and features
+class Game {
+    constructor() {
+        // Physics updates run at a fixed frequency, independent of rendering
+        // frame rate.
+        this.physicsPerSecond = 25;
+        this.physicsFrameTime = 1000 / this.physicsPerSecond;
+        this.framesBeforeDrop = 5;
 
-    let startTime = performance.now();
-    let lastTime = startTime;
-    let physicsFrameTimeAccumulator = startTime;
+        this.startTime = performance.now();
+        this.lastTime = this.startTime;
+        this.physicsFrameTimeAccumulator = this.startTime;
 
-    // Performance metrics
-    let skippedUpdates = 0;
-    let multiUpdates = 0;
-    let maxMultiUpdate = 0;
+        // Performance metrics
+        this.skippedUpdates = 0;
+        this.multiUpdates = 0;
+        this.maxMultiUpdate = 0;
 
-    // world
-    const physicsSystem = new PhysicsSystem({minX: 0, minY:0, maxX:100, maxY:100});
-    
-    const players = [
-        createPlayerFromConfig(physicsSystem, {
-            "render": {
-                "asset":        "tri",
-                "size":         10
-            },
-            "physics": {
-                "position": {"x": 30, "y": 50},
-                "boundingRadius":   5,
-                "mass":         10,
-                "rotateRate":   10,
-                "maxSpeed":     10,
-                "thrusters": [
-                    {"power": 10, "angle": 0},
-                    {"power": 10, "angle": 180}
-                ]
-            },
-            "control": {
-                "thrust":       [Keyboard.KEY_UP, Keyboard.KEY_DOWN],
-                "rotate_left":  Keyboard.KEY_LEFT,
-                "rotate_right": Keyboard.KEY_RIGHT,
-                "tractor":      Keyboard.KEY_SPACE
-            }
-        }),
-        createPlayerFromConfig(physicsSystem, {
-            "render": {
-                "asset":        "tri",
-                "size":         5
-            },
-            "physics": {
-                "position": {"x": 60, "y": 50},
-                "boundingRadius":   2.5,
-                "mass":         5,
-                "rotateRate":   20,
-                "maxSpeed":     10,
-                "thrusters": [
-                    {"power": 10, "angle": 0}
-                ]
-            },
-            "control": {
-                "thrust":       [Keyboard.KEY_W],
-                "rotate_left":  Keyboard.KEY_A,
-                "rotate_right": Keyboard.KEY_D
-            }
-        })
-    ];
-
-    // fix targetting for now since there's only two ships
-    players[0].target = players[1];
-    players[1].target = players[0];
-
-    for (player of players) {
-        physicsSystem.add(player.physicsObject);
+        // world
+        this.physicsSystem = new PhysicsSystem({minX: 0, minY:0, maxX:100, maxY:100});
     }
 
-    window.addEventListener('keydown', Keyboard.keydown_handler);
-    window.addEventListener('keyup', Keyboard.keyup_handler);
-
-    window.requestAnimationFrame(gameloop);
-
-    function teardown() {
-        window.removeEventListener('keydown', Keyboard.keydown_handler);
-        window.removeEventListener('keyup', Keyboard.keyup_handler);
+    run() {
     }
 
-    function gameloop(time) {
-        const timeSinceStart = time - startTime;
+    end() {
+    }
+
+    update(physicsFrameTime) {
+    }
+
+    render(interpolate) {
+    }
+
+    gameloop(time) {
+        const timeSinceStart = time - this.startTime;
 
         if (timeSinceStart <= 60000) {
             // run for one minute
-            window.requestAnimationFrame(gameloop);
+            window.requestAnimationFrame(this.gameloop.bind(this));
         } else {
-            console.log(`skipped updates (good) = ${skippedUpdates}`);
-            console.log(`multi updates (bad) = ${multiUpdates}`);
-            console.log(`max multi update = ${maxMultiUpdate}`);    
-            teardown();
+            console.log(`skipped updates (good) = ${this.skippedUpdates}`);
+            console.log(`multi updates (bad) = ${this.multiUpdates}`);
+            console.log(`max multi update = ${this.maxMultiUpdate}`);    
+            this.end();
             return;
         }
 
-        const frameTime = time - lastTime;
-        lastTime = time;
+        const frameTime = time - this.lastTime;
+        this.lastTime = time;
 
         let loops = 0;
-        while (performance.now() > physicsFrameTimeAccumulator && loops < framesBeforeDrop) {
+        while (performance.now() > this.physicsFrameTimeAccumulator && loops < this.framesBeforeDrop) {
             // using a frameTime less than the full physicsFrameTime to get the 
             // hands behaving properly when an absurdly low physics fps is set.
             // Wouldn't bother in a proper game loop with a high physics fps.
-            update(physicsFrameTime);
-            physicsFrameTimeAccumulator += physicsFrameTime;
+            this.update(this.physicsFrameTime);
+            this.physicsFrameTimeAccumulator += this.physicsFrameTime;
             loops ++;
         }
 
         // Log some performanc metrics
-        if (loops === 0) skippedUpdates++;
+        if (loops === 0) this.skippedUpdates++;
         else if (loops > 1) {
-            multiUpdates++;
-            if (loops > maxMultiUpdate) maxMultiUpdate = loops;
+            this.multiUpdates++;
+            if (loops > this.maxMultiUpdate) this.maxMultiUpdate = loops;
         }
 
         // Run rendering update, pass the interpolate value, which indicates
         // (using a value between 0 and 1) how far between physics updates we are.
-        render((performance.now() + physicsFrameTime - physicsFrameTimeAccumulator) / physicsFrameTime);
+        this.render((performance.now() + this.physicsFrameTime - this.physicsFrameTimeAccumulator) / this.physicsFrameTime);
+    }
+}
+
+class SpaceGame extends Game {
+    constructor() {
+        super();
+
+        this.players = [
+            createPlayerFromConfig(this.physicsSystem, {
+                "render": {
+                    "asset":        "tri",
+                    "size":         10
+                },
+                "physics": {
+                    "position": {"x": 30, "y": 50},
+                    "boundingRadius":   5,
+                    "mass":         10,
+                    "rotateRate":   10,
+                    "maxSpeed":     10,
+                    "thrusters": [
+                        {"power": 10, "angle": 0},
+                        {"power": 10, "angle": 180}
+                    ]
+                },
+                "control": {
+                    "thrust":       [Keyboard.KEY_UP, Keyboard.KEY_DOWN],
+                    "rotate_left":  Keyboard.KEY_LEFT,
+                    "rotate_right": Keyboard.KEY_RIGHT,
+                    "tractor":      Keyboard.KEY_SPACE
+                }
+            }),
+            createPlayerFromConfig(this.physicsSystem, {
+                "render": {
+                    "asset":        "tri",
+                    "size":         5
+                },
+                "physics": {
+                    "position": {"x": 60, "y": 50},
+                    "boundingRadius":   2.5,
+                    "mass":         5,
+                    "rotateRate":   20,
+                    "maxSpeed":     10,
+                    "thrusters": [
+                        {"power": 10, "angle": 0}
+                    ]
+                },
+                "control": {
+                    "thrust":       [Keyboard.KEY_W],
+                    "rotate_left":  Keyboard.KEY_A,
+                    "rotate_right": Keyboard.KEY_D
+                }
+            })
+        ];
+
+        // fix targetting for now since there's only two ships
+        this.players[0].target = this.players[1];
+        this.players[1].target = this.players[0];
+
+        for (let player of this.players) {
+            this.physicsSystem.add(player.physicsObject);
+        }
     }
 
-    function update(time) {
-        for (player of players) {
+    run() {
+        window.addEventListener('keydown', Keyboard.keydown_handler);
+        window.addEventListener('keyup', Keyboard.keyup_handler);
+
+        window.requestAnimationFrame(this.gameloop.bind(this));
+    }
+
+    end() {
+        window.removeEventListener('keydown', Keyboard.keydown_handler);
+        window.removeEventListener('keyup', Keyboard.keyup_handler);
+    }
+
+    update(time) {
+        for (let player of this.players) {
             player.update();
         }
-        physicsSystem.update(physicsFrameTime);
+        this.physicsSystem.update(this.physicsFrameTime);
     }
 
-    function render(interoplate) {
-        for (player of players) {
+    render(interoplate) {
+        for (let player of this.players) {
             player.renderObject.rotate(player.physicsObject.rotateAngle);
             player.renderObject.moveTo(player.physicsObject.position.x, player.physicsObject.position.y);
         }
     }
+}
+
+window.onload = function() {
+    const game = new SpaceGame();
+    game.run();
 }
