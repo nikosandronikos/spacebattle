f0a9193a5e4efe5dabb921af803789f3d68e3cf6
Nikos Andronikos <dodgeyhack@gmail.com>
2017-01-05 21:50:50 +1100

    Add a method for supporting discrete key presses

--- src/input.js
+++ src/input.js
@@ -10,32 +10,51 @@ const Keyboard  = {
     "KEY_D": 68,
     "KEY_SPACE": 32,
     "keys": [],
+    "ignoreKeys": [],
     "nKeysDown": 0,
     "keydown_handler": function(evt) {
-        Keyboard.nKeysDown ++;
-        Keyboard.keys[evt.keyCode] = true; 
+        if (!Keyboard.ignoreKeys[evt.keyCode]) {
+            if (!Keyboard.keys[evt.keyCode]) Keyboard.nKeysDown ++;
+            Keyboard.keys[evt.keyCode] = true;
+        } 
+
         if (evt.keyCode == Keyboard.KEY_UP ||
             evt.keyCode == Keyboard.KEY_DOWN ||
             evt.keyCode == Keyboard.KEY_LEFT ||
             evt.keyCode == Keyboard.KEY_RIGHT
-        )
+        ) {
             evt.preventDefault();
+        }
     },
     "keyup_handler": function(evt) {
-        //TODO: Remove from array to minimise memory consumption.
-        this.nKeysDown --;
-        Keyboard.keys[evt.keyCode] = false;
+        // TODO: Remove from array to minimise memory consumption,
+        // or only track particular keys.
+        if (Keyboard.ignoreKeys[evt.keyCode]) {
+            Keyboard.ignoreKeys[evt.keyCode] = false;
+        } else {
+            if (Keyboard.keys[evt.keyCode]) Keyboard.nKeysDown --;
+            Keyboard.keys[evt.keyCode] = false;
+        }
+
         if (evt.keyCode == Keyboard.KEY_UP ||
             evt.keyCode == Keyboard.KEY_DOWN ||
             evt.keyCode == Keyboard.KEY_LEFT ||
             evt.keyCode == Keyboard.KEY_RIGHT
-        )
+        ) {
             evt.preventDefault();
+        }
     },
     "pressed": function(keyCode) {
         return Keyboard.keys[keyCode] === true;
     },
     "anyKeyDown": function() {
         return Keyboard.nKeysDown > 0;
+    },
+    "ignoreUntilReleased": function(keyCode) {
+        if (Keyboard.keys[keyCode]) {
+            Keyboard.nKeysDown --;
+            Keyboard.keys[keyCode] = false;
+            Keyboard.ignoreKeys[keyCode] = true;
+        }
     }
 }
