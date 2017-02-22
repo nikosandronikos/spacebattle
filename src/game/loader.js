77f788d2a5378a3c02aec045151d4f69b1c7d5be
Nikos Andronikos <dodgeyhack@gmail.com>
2017-02-22 21:04:20 +1100

    Remove tabs

--- src/game/loader.js
+++ src/game/loader.js
@@ -4,16 +4,16 @@ import {RendererAssets} from '../../assets/config.js';
 import {SpaceGame} from './spacegame';
 
 export const GameLoader = {
-	init: function() {
+    init: function() {
         Renderer.init(window.innerWidth, window.innerHeight);
-	},
+    },
 
-	loadAssets: function(onCompleteFn) {
+    loadAssets: function(onCompleteFn) {
         Renderer.loadAssets(RendererAssets, onCompleteFn);
-	},
+    },
 
-	start: function() {
-	    this.game = new SpaceGame(Renderer);
-	    this.game.run();
-	}
+    start: function() {
+        this.game = new SpaceGame(Renderer);
+        this.game.run();
+    }
 };
