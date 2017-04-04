5ba4e77ce64970c3c3bde653fe90366907e01884
Nikos Andronikos <dodgeyhack@gmail.com>
2017-04-04 16:30:55 +1000

    Reduce gravity effect of planet

--- src/game/props.js
+++ src/game/props.js
@@ -51,7 +51,7 @@ export class PlanetObject extends GameObject {
                     Vector2d.createFromPoints(
                         obj.physicsModel.position,
                         this.physicsModel.position
-                    ).normalise().multiply(Math.max(20)).multiply(obj.physicsModel.mass), //200, planetGravityDist - dist)),
+                    ).normalise().multiply(Math.max(2)).multiply(obj.physicsModel.mass), //200, planetGravityDist - dist)),
                     1000 / PHYSICS_PER_SECOND // one physics frame. FIXME: support as default?
                 );
             }
