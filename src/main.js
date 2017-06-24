70964297701248f4de9eca9c4b28bcc733db7c06
Nikos Andronikos <dodgeyhack@gmail.com>
2017-06-24 23:07:42 +1000

    Export various modules to the world.
    Mostly so they can be included in tests.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 export * from './game/loader';
 
+// Export additional modules so they can be included by tests.
+export * from './physics';
+
+export * from './2dGameUtils';
+
 global.SPACEBATTLE = exports;
 
